8ecada5cd2b4cdf3bcc0f545ee42056d0b6b5cca
Sistemas Cerízeon <sistemascerizeon@gmail.com>
2021-09-23 19:22:43 -0400

    demas rompecabezas

--- database/seeders/ResourcesTableSeeder.php
+++ database/seeders/ResourcesTableSeeder.php
@@ -14,6 +14,7 @@ class ResourcesTableSeeder extends Seeder
      */
     public function run()
     {
+        //Ballena
         DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Ballena/1.png','audio1',1]);
         DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Ballena/2.png','audio2',1]);
         DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Ballena/3.png','audio3',1]);
@@ -23,5 +24,96 @@ class ResourcesTableSeeder extends Seeder
         DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Ballena/7.png','audio7',1]);
         DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Ballena/8.png','audio8',1]);
         DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Ballena/9.png','audio9',1]);
+        //Camisa
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Camisa/1.png','audio1',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Camisa/2.png','audio2',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Camisa/3.png','audio3',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Camisa/4.png','audio4',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Camisa/5.png','audio5',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Camisa/6.png','audio6',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Camisa/7.png','audio7',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Camisa/8.png','audio8',2]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Camisa/9.png','audio9',2]);
+        //Corazon
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Corazon/1.png','audio1',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Corazon/2.png','audio2',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Corazon/3.png','audio3',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Corazon/4.png','audio4',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Corazon/5.png','audio5',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Corazon/6.png','audio6',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Corazon/7.png','audio7',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Corazon/8.png','audio8',3]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Corazon/9.png','audio9',3]);
+        //Estrella
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Estrella/1.png','audio1',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Estrella/2.png','audio2',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Estrella/3.png','audio3',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Estrella/4.png','audio4',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Estrella/5.png','audio5',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Estrella/6.png','audio6',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Estrella/7.png','audio7',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Estrella/8.png','audio8',4]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Estrella/9.png','audio9',4]);
+        //Flor
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Flor/1.png','audio1',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Flor/2.png','audio2',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Flor/3.png','audio3',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Flor/4.png','audio4',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Flor/5.png','audio5',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Flor/6.png','audio6',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Flor/7.png','audio7',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Flor/8.png','audio8',5]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Flor/9.png','audio9',5]);
+        //Lazo
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Lazo/1.png','audio1',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Lazo/2.png','audio2',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Lazo/3.png','audio3',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Lazo/4.png','audio4',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Lazo/5.png','audio5',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Lazo/6.png','audio6',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Lazo/7.png','audio7',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Lazo/8.png','audio8',6]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Lazo/9.png','audio9',6]);
+        //Manzana
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Manzana/1.png','audio1',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Manzana/2.png','audio2',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Manzana/3.png','audio3',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Manzana/4.png','audio4',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Manzana/5.png','audio5',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Manzana/6.png','audio6',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Manzana/7.png','audio7',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Manzana/8.png','audio8',7]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Manzana/9.png','audio9',7]);
+        //Pasto
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Pasto/1.png','audio1',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Pasto/2.png','audio2',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Pasto/3.png','audio3',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Pasto/4.png','audio4',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Pasto/5.png','audio5',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Pasto/6.png','audio6',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Pasto/7.png','audio7',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Pasto/8.png','audio8',8]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Pasto/9.png','audio9',8]);
+        //Pera
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Pera/1.png','audio1',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Pera/2.png','audio2',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Pera/3.png','audio3',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Pera/4.png','audio4',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Pera/5.png','audio5',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Pera/6.png','audio6',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Pera/7.png','audio7',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Pera/8.png','audio8',9]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Pera/9.png','audio9',9]);
+        //Sol
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 1','http://147.182.226.120/game/Images/Sol/1.png','audio1',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 2','http://147.182.226.120/game/Images/Sol/2.png','audio2',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 3','http://147.182.226.120/game/Images/Sol/3.png','audio3',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 4','http://147.182.226.120/game/Images/Sol/4.png','audio4',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 5','http://147.182.226.120/game/Images/Sol/5.png','audio5',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 6','http://147.182.226.120/game/Images/Sol/6.png','audio6',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 7','http://147.182.226.120/game/Images/Sol/7.png','audio7',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 8','http://147.182.226.120/game/Images/Sol/8.png','audio8',10]);
+        DB::insert('insert into recursos (Label, Image, Audio, rompecabeza_id) values (?, ?, ?, ?)', ['Pieza 9','http://147.182.226.120/game/Images/Sol/9.png','audio9',10]);
     }
+
 }
