3c605a6676005919973041ae25717392bd6874dc
Sistemas Cerízeon <sistemascerizeon@gmail.com>
2021-09-25 13:23:06 -0400

    recursos

--- app/Http/Controllers/RompecabezaController.php
+++ app/Http/Controllers/RompecabezaController.php
@@ -5,15 +5,11 @@ use Illuminate\Support\Facades\DB;
 
 class RompecabezaController extends Controller
 {
-    
+
     public function show($id)
     {
-        $grupo = array();
-       
-        $recursos = app('db')->select("SELECT * FROM `recursos` WHERE `rompecabeza_id` = $id");
-      
-    
-        return '"array":'. json_encode($recursos);
+       $recursos = app('db')->select("SELECT * FROM `recursos` WHERE `rompecabeza_id` = $id");
+        return json_encode($recursos);
     }
 
 }
