018aa7cdc6c8e83a9871ee4aa0e23376ad99d63c
Adrian Borthomiert <fritoadn@gmail.com>
2022-07-28 09:16:02 -0400

    cambio de dominio en servicios game

--- app/Http/Controllers/MochilaController.php
+++ app/Http/Controllers/MochilaController.php
@@ -12,7 +12,7 @@ class MochilaController extends Controller
        $pru=collect();
        $data2=collect([
         "Label"=>'Mochila',
-        "Image"=>'https://app.wabim.com/game/Mochila/Mochila.png',
+        "Image"=>'https://app.linguaplanet.com.mx/game/Mochila/Mochila.png',
         "Audio"=>"https://cdn.pixabay.com/download/audio/2021/08/04/audio_67d243357f.mp3?filename=whoosh-6316.mp3",
        ]);
        $pru->push($data2);
